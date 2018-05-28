d789576f220463c62e00a3f3634f93cd6e7c2d75
Zhuyi Xue <alfred532008@gmail.com>
2018-05-28 16:52:04 -0700

    added batch_size support for encoder

--- seq2sst3.py
+++ seq2sst3.py
@@ -19,30 +19,30 @@ print('device: {0}'.format(DEVICE))
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, embedding_size, hidden_size,
-                 num_layers=1, bidirectional=False, batch_size=1):
+    def __init__(self, vocab_size, embedding_size, hidden_size, num_layers=1,
+                 bidirectional=False):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.bidirectional = bidirectional
-        self.batch_size = batch_size
 
-        self.embedding = nn.Embedding(input_size, embedding_size)
-
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
         self.gru = nn.GRU(embedding_size, hidden_size, num_layers,
                           bidirectional=bidirectional)
 
     def forward(self, inputs, hidden):
-        seq_len = inputs.shape[0]
-        embedded = self.embedding(inputs).view(seq_len, 1, -1)
+        seq_len, batch_size, _ = inputs.shape
+        # https://pytorch.org/docs/stable/nn.html#torch.nn.Embedding
+        embedded = self.embedding(inputs).view(seq_len, batch_size, -1)
+        # https://pytorch.org/docs/stable/nn.html#torch.nn.GRU
         output, hidden = self.gru(embedded, hidden)
         return output, hidden
 
-    def init_hidden(self):
+    def init_hidden(self, batch_size):
         directions = 2 if self.bidirectional else 1
         return torch.zeros(
             self.num_layers * directions,
-            self.batch_size,
+            batch_size,
             self.hidden_size,
             device=DEVICE
         )
@@ -69,6 +69,7 @@ class AttnDecoderRNN(nn.Module):
         self.Ws = nn.Linear(hidden_size, output_size)
 
     def forward(self, input, hidden, encoder_outputs):
+        import pdb; pdb.set_trace()
         embedded = self.embedding(input).view(1, 1, -1)
         embedded = self.dropout(embedded)
 
@@ -107,14 +108,14 @@ def tensors_from_pair(src_lang, tgt_lang, pair):
     return (src_tensor, tgt_tensor, seq_len)
 
 
-def train(src_lang, tgt_lang, enc, dec, src_tensor, tgt_tensor, seq_len,
+def train(src_lang, tgt_lang, enc, dec, src_tensor, tgt_tensor, seq_lens,
           tgt_sos_index, enc_optim, dec_optim,
-          criterion, teacher_forcing_ratio=0.5):
+          criterion, teacher_forcing_ratio=0.5, batch_size=1):
     enc_optim.zero_grad()
     dec_optim.zero_grad()
 
     # encoding source sequence
-    enc_hid = enc.init_hidden()
+    enc_hid = enc.init_hidden(batch_size)
     directions = 2 if enc.bidirectional else 1
     enc_outs, enc_hid = enc(src_tensor, enc_hid)
 
@@ -128,6 +129,7 @@ def train(src_lang, tgt_lang, enc, dec, src_tensor, tgt_tensor, seq_len,
     use_tf = True if random.random() < teacher_forcing_ratio else False
 
     loss = 0
+    seq_len = max(seq_lens)
     if use_tf:
         for di in range(seq_len):
             dec_out, dec_hid, dec_attn = dec(dec_in, dec_hid, enc_outs)
@@ -152,7 +154,7 @@ def train(src_lang, tgt_lang, enc, dec, src_tensor, tgt_tensor, seq_len,
 
 
 def trainIters(src_lang, tgt_lang, enc, dec, tgt_sos_index, n_iters,
-               print_every=1000, plot_every=100, learning_rate=0.001):
+               batch_size=2, print_every=1000, plot_every=100, learning_rate=0.001):
     print('training for {0} steps'.format(n_iters))
     print('collect loss for plotting per {0} steps'.format(plot_every))
 
@@ -168,17 +170,30 @@ def trainIters(src_lang, tgt_lang, enc, dec, tgt_sos_index, n_iters,
 
     tr_pair_tensors = []
     for i in range(n_iters):
-        tsr = tensors_from_pair(src_lang, tgt_lang, random.choice(pairs))
-        tr_pair_tensors.append(tsr)
+        src_tsrs, tgt_tsrs, seq_lens = [], [], []
+        for j in range(batch_size):
+            _s, _t, _l = tensors_from_pair(
+                src_lang, tgt_lang, random.choice(pairs))
+            src_tsrs.append(_s)
+            tgt_tsrs.append(_t)
+            seq_lens.append(_l)
+            # TODO: padding should happen here
+        tr_pair_tensors.append([
+            # make sure it's (seq_len, batch, input_size)
+            torch.stack(src_tsrs, dim=1),
+            torch.stack(tgt_tsrs, dim=1),
+            torch.tensor(seq_lens),
+        ])
 
     criterion = nn.NLLLoss()
 
     for idx in range(1, n_iters + 1):
-        src_tensor, tgt_tensor, seq_len = tr_pair_tensors[idx - 1]
+        src_tensor, tgt_tensor, seq_lens = tr_pair_tensors[idx - 1]
 
         loss = train(
-            src_lang, tgt_lang, enc, dec, src_tensor, tgt_tensor, seq_len,
-            tgt_sos_index, enc_optim, dec_optim, criterion
+            src_lang, tgt_lang, enc, dec, src_tensor, tgt_tensor, seq_lens,
+            tgt_sos_index, enc_optim, dec_optim, criterion,
+            batch_size=batch_size
         )
         print_loss_total += loss
         plot_loss_total += loss
@@ -257,7 +272,8 @@ if __name__ == "__main__":
         src_lang.n_words,
         args.embedding_size,
         args.hidden_size,
-        bidirectional=False
+        num_layers=1,
+        bidirectional=False,
     )
     enc = enc.to(DEVICE)
 
@@ -270,4 +286,6 @@ if __name__ == "__main__":
     dec.to(DEVICE)
 
     trainIters(src_lang, tgt_lang, enc, dec, tgt_sos_index,
-               n_iters=args.num_iters, print_every=args.print_every)
+               n_iters=args.num_iters, print_every=args.print_every,
+               batch_size=args.batch_size
+    )
