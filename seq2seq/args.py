f3ed89b78431acbc786534bb99f63b2797d2692c
Zhuyi Xue <alfred532008@gmail.com>
2019-01-15 14:49:56 -0800

    wrote help for parse_args

--- seq2seq/args.py
+++ seq2seq/args.py
@@ -4,22 +4,50 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--input', type=str)
-    parser.add_argument('-e', '--embedding-size', type=int)
-    parser.add_argument('-d', '--hidden-size', type=int)
-    parser.add_argument('-b', '--batch-size', type=int, default=1)
-    parser.add_argument('-l', '--num-layers', type=int, default=1)
-    parser.add_argument('-r', '--bidirectional', action='store_true')
-    parser.add_argument('--learning-rate', type=float, default=0.01)
-    parser.add_argument('-o', '--outdir', type=str)
-
-    parser.add_argument('-t', '--num-iters', type=int, default=5000)
-    parser.add_argument('-p', '--print-every', type=int, default=100)
+    parser.add_argument(
+        '-i', '--input', type=str,
+        help='input file with training data'
+    )
+    parser.add_argument(
+        '-e', '--embedding-size', type=int,
+        help='the dimension of embeding vector'
+    )
+    parser.add_argument(
+        '-d', '--hidden-size', type=int,
+        help='hidden layer size'
+    )
+    parser.add_argument(
+        '-b', '--batch-size', type=int, default=1,
+        help='batch size'
+    )
+    parser.add_argument(
+        '-l', '--num-layers', type=int, default=1,
+        help='number of hidden layers'
+    )
+    parser.add_argument(
+        '-r', '--bidirectional', action='store_true',
+        help='use bidirectional RNN if set'
+    )
+    parser.add_argument(
+        '--learning-rate', type=float, default=0.01,
+        help='learning rate'
+    )
+    parser.add_argument(
+        '-o', '--outdir', type=str,
+        help='output directory'
+    )
+    parser.add_argument(
+        '-t', '--num-iters', type=int, default=5000,
+        help='number of iterations'
+    )
+    parser.add_argument(
+        '-p', '--print-every', type=int, default=100,
+        help='print interval'
+    )
 
     parser.add_argument(
         '--plot-every', type=int, default=0,
-        help='if 0, no plotting willl be done'
+        help='if 0, no plots willl be generated'
     )
 
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
