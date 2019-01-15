eb74c0f782583f6c47f31fcce7f48bd33a9390f4
Zhuyi Xue <alfred532008@gmail.com>
2019-01-15 15:47:49 -0800

    refactored seq2seq/encoder.py

--- seq2seq/encoder.py
+++ seq2seq/encoder.py
@@ -3,23 +3,27 @@ import torch.nn as nn
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, vocab_size, embedding_size, hidden_size, num_layers,
+    def __init__(self, num_tokens, embedding_dim, hidden_size, num_layers,
                  bidirectional=False):
         super(EncoderRNN, self).__init__()
+
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.bidirectional = bidirectional
 
-        self.embedding = nn.Embedding(vocab_size, embedding_size)
-        self.gru = nn.GRU(embedding_size, hidden_size, num_layers,
+        self.embedding = nn.Embedding(num_tokens, embedding_dim)
+        self.gru = nn.GRU(embedding_dim, hidden_size, num_layers,
                           bidirectional=bidirectional)
 
     def forward(self, inputs, hidden):
         seq_len, batch_size, _ = inputs.shape
+
         # https://pytorch.org/docs/stable/nn.html#torch.nn.Embedding
         embedded = self.embedding(inputs).view(seq_len, batch_size, -1)
+
         # https://pytorch.org/docs/stable/nn.html#torch.nn.GRU
         output, hidden = self.gru(embedded, hidden)
+
         return output, hidden
 
     def init_hidden(self, batch_size, device):
