bb3053a4580ca841911e408e0cc69b6f3cedc793
Zhuyi Xue <alfred532008@gmail.com>
2019-01-16 11:30:22 -0800

    updated Language class

--- seq2seq/objs.py
+++ seq2seq/objs.py
@@ -9,10 +9,13 @@ class Language(object):
         """
         self.name = name
         # convention: vocabulary doesn't include beg/end/unk tokens
-        self.vocab = set(vocab)
+        self.vocab = sorted(set(vocab))
         self.beg_token = beg_token
         self.end_token = end_token
         self.unk_token = unk_token
+
+        # tokens include every character while vocab doesn't
+        self.tokens = [beg_token, end_token, unk_token] + self.vocab
 
         self.init_indices()
 
@@ -24,15 +27,21 @@ class Language(object):
 
     def gen_t2i_dict(self):
         dd = {}
-        sorted_tokens = [self.beg_token,
-                         self.end_token,
-                         self.unk_token] + list(sorted(self.vocab))
-        for k, i in enumerate(sorted_tokens):
+        for k, i in enumerate(self.tokens):
             dd[i] = k
         return dd
 
     def gen_i2t_dict(self):
         return {j: i for (i, j) in self.token2index.items()}
+
+    def to_dict(self):
+        return dict(
+            name=self.name,
+            vocab=self.vocab,
+            beg_token=self.beg_token,
+            end_token=self.end_token,
+            unk_token=self.unk_token
+        )
 
     def __repr__(self):
         return f'Language("{self.name}", "{self.vocab}")'
