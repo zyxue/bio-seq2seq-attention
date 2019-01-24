7e6bd2f1e4d3a753311254bee242d298e9e086c2
Zhuyi Xue <alfred532008@gmail.com>
2019-01-24 12:56:45 -0800

    debugged decoder.MLPDecoder

--- seq2seq/decoder.py
+++ seq2seq/decoder.py
@@ -5,13 +5,15 @@ import torch.nn.functional as F
 
 class MLPDecoder(nn.Module):
     def __init__(self, language, hidden_size, num_layers):
-        super(MLP, self).__init__()
+        super(MLPDecoder, self).__init__()
+        self.language = language
+
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.output_size = language.num_tokens
 
         self.W_hidden_list = [
-            nn.Linear(self.hidden_size, self.hidden_size) for _ in num_layers
+            nn.Linear(self.hidden_size, self.hidden_size) for _ in range(num_layers)
         ]
         self.W_out = nn.Linear(self.hidden_size, self.output_size)
 
